639deb46cee029f40d32cdb9fb4b8fffdde4c0b4
xinle <xingle@sportsdata.cn>
2017-09-01 11:35:20 +0800

    add files 初始化和清除

---
+++ src/main/java/com/springmvc/base/SimpleConstructor.java
@@ -0,0 +1,14 @@
+package com.springmvc.base;
+
+class Rock {
+	Rock(int i) { // This is the constructor
+		System.out.println("Creating Rock number " + i);
+	}
+}
+
+public class SimpleConstructor {
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++)
+			new Rock(i);
+	}
+}
