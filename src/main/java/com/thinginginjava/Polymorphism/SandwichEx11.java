152641d6c323efcd14d237730637eae50ab00e69
xingle <xinle@sportsdata.cn>
2017-09-20 07:43:51 +0800

    Polymorphism, Exercise 11

---
+++ src/main/java/com/thinginginjava/Polymorphism/SandwichEx11.java
@@ -0,0 +1,14 @@
+// polymorphism/SandwichEx11.java
+// TIJ4 Chapter Polymorphism, Exercise 11, page 159
+// Add class Pickle to Sandwich.java
+package com.thinginginjava.Polymorphism;
+
+class Meal {
+	Meal() {
+		System.out.println("Meal()");
+	}
+}
+
+public class SandwichEx11 {
+
+}
