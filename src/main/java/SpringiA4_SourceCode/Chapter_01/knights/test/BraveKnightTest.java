ab0447ecc766c1b765883c58624ead3a62c22efc
xinle <xingle@sportsdata.cn>
2017-09-19 10:54:05 +0800

    edit file

--- src/main/java/SpringiA4_SourceCode/Chapter_01/knights/test/BraveKnightTest.java
+++ src/main/java/SpringiA4_SourceCode/Chapter_01/knights/test/BraveKnightTest.java
@@ -1,6 +1,9 @@
 package SpringiA4_SourceCode.Chapter_01.knights.test;
 
 import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 import static org.mockito.Mockito.*;
 
 import SpringiA4_SourceCode.Chapter_01.knights.BraveKnight;
@@ -13,6 +16,11 @@ public class BraveKnightTest {
 		BraveKnight knight = new BraveKnight(mockQuest);
 		knight.embarkOnQuest();
 		verify(mockQuest, times(1)).embark();
+		
+		FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("");
+		
+		
+		ClassPathXmlApplicationContext c = new ClassPathXmlApplicationContext("");
 	}
 }
 
