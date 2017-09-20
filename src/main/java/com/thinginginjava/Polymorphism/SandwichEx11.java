9cfea39d4672c4c2d5204578b56c9e14f1acc1ad
xinle <xingle@sportsdata.cn>
2017-09-21 07:24:55 +0800

    edit Polymorphism, Exercise 11

--- src/main/java/com/thinginginjava/Polymorphism/SandwichEx11.java
+++ src/main/java/com/thinginginjava/Polymorphism/SandwichEx11.java
@@ -1,14 +1,43 @@
 // polymorphism/SandwichEx11.java
 // TIJ4 Chapter Polymorphism, Exercise 11, page 159
-// Add class Pickle to Sandwich.java
+// 向 Sandwich.java 中添加 Pickle类
 package com.thinginginjava.Polymorphism;
 
 class Meal {
-	Meal() {
-		System.out.println("Meal()");
-	}
+	Meal() { System.out.println("Meal()"); } 
+}
+
+class Bread {
+	Bread() { System.out.println("Bread()"); } 
+}
+
+class Cheese {
+	Cheese() { System.out.println("Cheese()"); } 
+}
+
+class Lettuce {
+	Lettuce() { System.out.println("Lettuce()"); } 
+}
+//新添加Pickle类
+class Pickle {
+	Pickle() { System.out.println("Pickle()"); }
+}
+
+class Lunch extends Meal {
+	Lunch() { System.out.println("Lunch()"); } 
+}
+
+class PortableLunch extends Lunch {
+	PortableLunch() { System.out.println("PortableLunch()"); }
 }
 
 public class SandwichEx11 {
-
+	private Bread b = new Bread();
+	private Cheese c = new Cheese();
+	private Pickle p = new Pickle();
+	private Lunch l = new Lunch();
+	public SandwichEx11() { System.out.println("Sandwich()"); }
+	public static void main(String[] args){
+		new SandwichEx11();
+	}
 }
