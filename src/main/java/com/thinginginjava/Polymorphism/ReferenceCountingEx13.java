7389c6387fd6313fbae81c79faba3dbc87b8fbea
xinle <xingle@sportsdata.cn>
2017-09-21 13:39:33 +0800

    Polymorphism, Exercise 13

---
+++ src/main/java/com/thinginginjava/Polymorphism/ReferenceCountingEx13.java
@@ -0,0 +1,10 @@
+// polymorphism/ReferenceCountingEx13.java
+// TIJ4 Chapter Polymorphism, Exercise 13, page 162
+/* 在 ReferenceCounting.java 中添加一个finalize()方法，
+ * 用来校验终止条件（查看第五章 初始化和清理）
+*/
+package com.thinginginjava.Polymorphism;
+
+public class ReferenceCountingEx13 {
+
+}
