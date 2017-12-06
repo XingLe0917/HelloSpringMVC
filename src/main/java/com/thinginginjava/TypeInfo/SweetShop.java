42077033635db4c80d74b3a5f178a9e8b801f902
xingle <le.xing@tinno.com>
2017-12-06 08:16:53 +0800

    test

--- src/main/java/com/thinginginjava/TypeInfo/SweetShop.java
+++ src/main/java/com/thinginginjava/TypeInfo/SweetShop.java
@@ -1,37 +1,44 @@
 package com.thinginginjava.TypeInfo;
+
 class Candy {
+
   static {
     System.out.println("Loading Candy");
   }
 }
 
 class Gum {
+
   static {
     System.out.println("Loading Gum");
   }
 }
 
 class Cookie {
+
   static {
     System.out.println("Loading Cookie");
   }
 }
 
 public class SweetShop {
-  public static void main(String[] args){
+
+  public static void main(String[] args) {
     System.out.println("inside main");
     new Candy();
     System.out.println("After creating Candy");
     try {
-      Class.forName("Gum");
+      Class c = Class.forName("com.thinginginjava.TypeInfo.Gum");
+      System.out.println(
+          "Class name :" + c.getName() + " is interface ?" + c.isInterface() + ",Simple name :" + c
+              .getSimpleName());
     } catch (ClassNotFoundException e) {
       System.out.println("Couldn't find Gum");
     }
     System.out.println("After Class.forName(\"Gum\")");
     new Cookie();
-    System.out.println("After creating Cookie");
+    System.out.println("After creating Cookie  ");
   }
-
 
 
 }
